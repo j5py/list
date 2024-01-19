004ab415651d220dad26f2eb6f9ac59b2f61fe86
j5py <j5py@proton.me>
2024-01-19 04:49:10 +0100

    Start with Class Inheritance, Multiple Inheritance, Decorator

--- RUNME.py
+++ RUNME.py
@@ -2,197 +2,231 @@
 import re
 import sys
 
-
-
-#-----> CONFIGURABLE
-
-where_to_find_new_urls = 'FEEDME.txt'
-# list the URLs to add, one per line
-
-where_to_save_all_changes = 'README.md'
-# alphabetical list of unique clickables
+where_potential_urls_can_be_proposed_one_per_line = 'FEEDME.txt'
+where_new_valid_urls_are_stored_alphabetically = 'README.md'
 
 
 
-#-----> USABLE
+class File:
 
-addables = list()
-readables = list()
-deletables = list()
+    @staticmethod
+    def rewrite(file, prefix = '', entries = False, line_feed = False):
+        with open(file, 'w') as opened:
+            if entries:
+                for each in entries:
+                    opened.write(prefix + each + '\n' if line_feed else '')
+            else:
+                opened.write('')
+            opened.close()
 
-url_pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[\w\-/?=&.:]*'
-
-def rewrite(file, prefix = '', lines = False, suffix = '\n'):
-    with open(file, 'w') as opened:
-        if lines:
-            for line in lines:
-                opened.write(prefix + line + suffix)
-        else:
-            opened.write('')
+    @staticmethod
+    def get_processed_entries(file, invokable_process):
+        entries = list()
+        opened = open(file)
+        for each in opened:
+            each = invokable_process(each.strip('\n'))
+            if each:
+                entries.append(each)
         opened.close()
+        return entries
 
-def get_list_of_unique(entries, alphabetical = True):
-    entries = list(set(entries))
-    if alphabetical:
-        return sorted(entries)
-    return entries
 
-def get_divider(length, symbol, string = '', i = 0):
-    while i < length:
-        string += symbol
-        i += 1
-    return string
 
-def get_margin(length, string = '', i = 0):
-    while i < length:
-        string += ' '
-        i += 1
-    return string
+class Readme:
 
-# ( i ) See class or namespace to group the following functions and highlight their interdependency
+    @staticmethod
+    def __init__():
+        Readme.line_break = '<br />'
 
-def get_list_from(file, invokable, pattern, sensitive, match): # invokable = get_valid
-    results = list()
-    opened = open(file)
-    for line in opened:
-        result = invokable(line, pattern, sensitive, match)
-        if result:
-            results.append(result)
-    opened.close()
-    return results
+    @staticmethod
+    def undo_as_line(string):
+        return string.replace(Readme.line_break, '')
 
-def get_valid(string, pattern, sensitive, match):
-    try:
-        if sensitive:
-            string = re.search(pattern, string).group(match)
-        else:
-            string = re.search(pattern, string, re.IGNORECASE).group(match)
+
+
+class Pattern:
+
+    @staticmethod
+    def __init__():
+        Pattern.regex = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[\w\-/?=&.:]*'
+
+    @staticmethod
+    def clean(string):
         return string.strip(' /')
-    except:
-        return None
 
-# ( i ) See class or namespace to group the following functions and highlight their interdependency
-
-def print_manual(margin, instructions):
-    print_separator(built_main_divider, True)
-    print(f'{margin}HELP {instructions}')
-    print_separator(built_main_divider, False)
-
-def print_status(margin, message, initial = False):
-    if initial:
-        print_separator(built_sub_divider,)
-    print(("\n" if initial else "") + margin + message)
-    if not initial:
-        print_separator(built_sub_divider,)
-
-def print_separator(divider, prefix = None):
-    if prefix is True:
-        print('\n' + divider)
-    elif prefix is False:
-        print(divider + '\n')
-    elif prefix is None:
-        print(divider)
-
-def get_input(margin, question):
-    string = input(margin + question)
-    print_separator(built_sub_divider,)
-    return string
-
-
-
-#-----> INITIAL
-
-built_main_divider = get_divider(48, '=')
-built_sub_divider = get_divider(48, '-')
-built_margin = get_margin(4)
-
-argument = len(sys.argv) > 1
-
-if argument:
-    for key in sys.argv[1:]:
-        key = get_valid(key, url_pattern, True, 0)
-        if key:
-            addables.append(key)
-else:
-    addables = get_list_from(
-        where_to_find_new_urls,
-        get_valid,
-        url_pattern,
-        True,
-        0
-    )
-    
-readables = get_list_from(
-    where_to_save_all_changes,
-    get_valid,
-    '<br />(.+)',
-    False,
-    1
-)
-
-previous = len(readables)
-readables = get_list_of_unique(readables + addables)
-update = len(readables)
-
-rewrite(where_to_save_all_changes, '<br />', readables)
-rewrite(where_to_find_new_urls)
-
-if update > previous:
-    if argument:
-        done = f'Command line argument{"s" if update - previous > 1 else ""}'
-        done += f' moved in {where_to_save_all_changes}'
-    else:
-        done = f'Content of {where_to_find_new_urls}'
-        done += f' moved to {where_to_save_all_changes}'
-else:
-    if argument:
-        done = f'There was no valid new URL passed as an argument'
-    else:
-        done = f'There was no valid new URL in {where_to_find_new_urls}'
-
-print_status(built_margin, done, True)
-
-
-
-#-----> INTERACTIVE
-
-print_manual(built_margin, '"x" ends, "examples\.io" searches')
-
-while True:
-    choice = get_input(built_margin, 'Something to delete? ')
-
-    if choice == 'x':
-        print_status(built_margin, 'End of execution')
-        quit()
-
-    for key in readables:
+    @staticmethod
+    def valid_url(string):
         try:
-            deletables.append(re.search(f'(.*)?{choice}(.*)?', key).group(0))
+            match = re.search(Pattern.regex, string).group(0)
+            return Pattern.clean(match)
         except:
-            continue
+            return None
 
-    if len(deletables):
-        for key in deletables:
-            print(built_margin + key)
 
-        print_manual(built_margin, '"x" ends, "v" valids, other cancels')
 
-        choice = get_input(built_margin, 'Do you confirm deletion? ')
+class List:
 
-        if choice == 'v':
-            for key in deletables:
-                readables.remove(key)
-            rewrite(where_to_save_all_changes, '<br />', readables)
-            rewrite(where_to_find_new_urls)
-            print_status(built_margin, 'Deletion done')
+    def __init__(boot):
+        boot.addables = list()
+        boot.readables = list()
+        boot.deletables = list()
+
+    @staticmethod
+    def get_unique(entries, alphabetical = True):
+        entries = list(set(entries))
+        if alphabetical:
+            return sorted(entries)
+        return entries
+
+
+
+class Interface:
+
+    @staticmethod
+    def __init__():
+        Interface.main_divider = Interface.get_divider(64, '=')
+        Interface.sub_divider = Interface.get_divider(64, '-')
+        Interface.margin = Interface.get_margin(4)
+
+    def get_divider(length, symbol, string = '', i = 0):
+        while i < length:
+            string += symbol
+            i += 1
+        return string
+
+    def get_margin(length, string = '', i = 0):
+        while i < length:
+            string += ' '
+            i += 1
+        return string
+
+    @staticmethod
+    def demarcate(divider, prefix = None):
+        if prefix is True:
+            print('\n' + divider)
+        elif prefix is False:
+            print(divider + '\n')
+        elif prefix is None:
+            print(divider)
+
+    @staticmethod
+    def help(instructions):
+        Interface.demarcate(Interface.main_divider, True)
+        print(f'{Interface.margin}HELP {instructions}')
+        Interface.demarcate(Interface.main_divider, False)
+
+    @staticmethod
+    def display(text, initial = False):
+        if initial:
+            Interface.demarcate(Interface.sub_divider)
+        print(("\n" if initial else "") + Interface.margin + text)
+        if not initial:
+            Interface.demarcate(Interface.sub_divider)
+
+
+
+class Persistence(List, Readme, File):
+    def update(boot):
+        File.rewrite(where_potential_urls_can_be_proposed_one_per_line)
+        File.rewrite(
+            where_new_valid_urls_are_stored_alphabetically,
+            Readme.line_break,
+            boot.readables,
+            True
+        )
+
+
+
+class Interactive(Persistence, Interface, List):
+
+    def ask(question):
+        return input(Interface.margin + question)
+
+    def run(boot):
+        Interface.help('"q" ends, "example\.io" searches')
+        while True:
+            it = Interactive.ask('Something to delete? ')
+            if 'q' == it:
+                Interface.display('End of execution')
+                quit()
+            for each in boot.readables:
+                try:
+                    boot.deletables.append(
+                        re.search(f'(.*)?{it}(.*)?', each).group(0)
+                    )
+                except:
+                    continue
+            if len(boot.deletables):
+                for each in boot.deletables:
+                    print(Interface.margin + each)
+                Interface.help('"q" ends, "y" confirms, any other choice cancels')
+                it = Interactive.ask('Do you confirm deletion? ')
+                if 'y' == it:
+                    for each in boot.deletables:
+                        boot.readables.remove(each)
+                    Persistence.update(boot)
+                    Interface.display('Deletion done')
+                else:
+                    Interface.display('Deletion canceled')
+                if 'q' == it:
+                    Interface.display('End of execution')
+                    quit()
+                boot.deletables = list()
+            else:
+                Interface.display('No match')
+
+
+
+class Boot(Interactive, Persistence, Interface, List, Pattern, Readme, File):
+
+    def __init__(self):
+        self.argument = len(sys.argv) > 1
+        Readme.__init__()
+        Pattern.__init__()
+        List.__init__(self)
+        Interface.__init__()
+        self.fetch_data()
+        self.process_data()
+        Persistence.update(self)
+        self.update_report()
+        Interactive.run(self)
+
+    def fetch_data(self):
+        if self.argument:
+            for each in sys.argv[1:]:
+                each = Pattern.valid_url(each)
+                if each:
+                    self.addables.append(each)
         else:
-            print_status(built_margin, 'Deletion canceled')
+            self.addables = File.get_processed_entries(
+                where_potential_urls_can_be_proposed_one_per_line,
+                Pattern.valid_url
+            )
+        self.readables = File.get_processed_entries(
+            where_new_valid_urls_are_stored_alphabetically,
+            Readme.undo_as_line
+        )
 
-        if choice == 'x':
-            print_status(built_margin, 'End of execution')
-            quit()
+    def process_data(self):
+        self.previous = len(self.readables)
+        self.readables = List.get_unique(self.readables + self.addables)
+        self.updated = len(self.readables)
 
-        deletables = list()
-    else:
-        print_status(built_margin, 'No match')
-        
+    def update_report(self):
+        if self.updated > self.previous:
+            if self.argument:
+                up = f'Command line argument{"s" if self.updated - self.previous > 1 else ""}'
+                up += f' moved in {where_new_valid_urls_are_stored_alphabetically}'
+            else:
+                up = f'Content of {where_potential_urls_can_be_proposed_one_per_line}'
+                up += f' moved to {where_new_valid_urls_are_stored_alphabetically}'
+        else:
+            if self.argument:
+                up = f'There was no valid new URL passed as an argument'
+            else:
+                up = f'There was no valid new URL in {where_potential_urls_can_be_proposed_one_per_line}'
+        Interface.display(up, True)
+
+
+
+start = Boot()
